5aa5f67b8c40504bdfd1a82c92f7154655a4fa9b
juli matta <julhacolucci@gmail.com>
2024-10-30 21:32:54 -0300

    Updated buttons in header and Operacao

--- components/Operacao.js
+++ components/Operacao.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 
 export default function Operacao({ price }) {
@@ -10,58 +10,75 @@ export default function Operacao({ price }) {
         R${price * num} / {num} ações
       </Text>
       <View style={styles.opBotoes}>
-        <Button
+        <TouchableOpacity
+          style={styles.btn}
           onPress={() => {
             setNum(num - 100);
-          }}
-          title="-100"
-        />
-        <Button
+          }}>
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>-100</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.btn}
           onPress={() => {
-            setNum(num - 10);
-          }}
-          title="-"
-        />
-        <Button
+            setNum(num - 1);
+          }}>
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>-1</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.btn}
           onPress={() => {
             setNum(1);
-          }}
-          title="reset"
-        />
-        <Button
+          }}>
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>reset</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.btn}
           onPress={() => {
-            setNum(num + 10);
-          }}
-          title="+"
-        />
-        <Button
+            setNum(num + 1);
+          }}>
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>+1</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.btn}
           onPress={() => {
             setNum(num + 100);
-          }}
-          title="+100"
-        />
+          }}>
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>+100</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     padding: '16px',
     backgroundColor: 'lightgray',
     margin: '18px',
-    borderRadius: '6px'
-    
+    borderRadius: '6px',
   },
   cardTitle: {
     fontSize: '20px',
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: '12px'
+    marginBottom: '12px',
   },
   opBotoes: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-  }
+  },
+  btn: {
+    padding: '0.25rem 0.5rem',
+    backgroundColor: '#5D5D5D',
+    borderRadius: '3px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+  },
 });
